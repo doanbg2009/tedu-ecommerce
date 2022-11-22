70a7444fdad13c0b40c57ddfc6d9b712f2c5c9fd
Nguyễn Đoán <doanbg2009@gmail.com>
2022-11-22 17:23:25 +0700

    Bài 4.1-4.5

---
+++ aspnet-core/src/Ecommerce.Domain.Shared/Attributes/AttributeType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Attributes
+{
+    public enum AttributeType
+    {
+        Date,
+        Varchar,
+        Text,
+        Int,
+        Decimal
+    }
+}
